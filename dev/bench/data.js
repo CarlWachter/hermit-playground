1dec578fade9060558c436c8678d246ea35f6a8a
github-action-benchmark <github@users.noreply.github.com>
2024-07-15 15:03:48 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 285f28af2b077a85723376c82ad8e38f508716d1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721055277724,
+  "lastUpdate": 1721055828056,
   "repoUrl": "https://github.com/CarlWachter/hermit-playground",
   "entries": {
     "Benchmark": [
@@ -422,6 +422,68 @@ window.BENCHMARK_DATA = {
           {
             "name": "libhermit.a",
             "value": 13.58388900756836,
+            "unit": "MB",
+            "range": 0,
+            "group": "File Size",
+            "plot_group": "Benchmark File Size"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Martin Kröning",
+            "username": "mkroening",
+            "email": "mkroening@posteo.net"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "285f28af2b077a85723376c82ad8e38f508716d1",
+          "message": "Merge pull request #1237 from CarlWachter/no_lwip\n\nfeat(newlib): using kernel networking, instead of lwip",
+          "timestamp": "2024-06-04T10:40:31Z",
+          "url": "https://github.com/hermit-os/kernel/commit/285f28af2b077a85723376c82ad8e38f508716d1"
+        },
+        "date": 1721055827307,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Build",
+            "value": 82.712610114,
+            "unit": "s",
+            "range": 0,
+            "group": "General",
+            "plot_group": "none"
+          },
+          {
+            "name": "1 core",
+            "value": 0.080988199,
+            "unit": "s",
+            "range": 0,
+            "group": "General",
+            "plot_group": "Startup Time"
+          },
+          {
+            "name": "2 cores",
+            "value": 0.08108245,
+            "unit": "s",
+            "range": 0,
+            "group": "General",
+            "plot_group": "Startup Time"
+          },
+          {
+            "name": "4 cores",
+            "value": 0.08283596,
+            "unit": "s",
+            "range": 0,
+            "group": "General",
+            "plot_group": "Startup Time"
+          },
+          {
+            "name": "libhermit.a",
+            "value": 13.584102630615234,
             "unit": "MB",
             "range": 0,
             "group": "File Size",
