6c33c8acc777849c6cce6e002b8b9fb108b266a3
CarlWachter <carlwachter@web.de>
2024-07-15 14:43:58 +0200

    bench: multithreaded pi

--- usr/benchmarks/bench_output.c
+++ usr/benchmarks/bench_output.c
@@ -1,11 +1,11 @@
-#include <bench_output.h>
+#include "bench_output.h"
 
 /// Log benchmark data to stdout, so it can be captured by the benchmarking framework hermit-bench.
 ///
 /// * `benchmark_name` - The name under which the benchmark will be stored.
 /// * `unit` - The unit of the benchmark value.
 /// * `value` - The benchmark value.
-void log_benchmark_data(const char* benchmark_name, const char* unit, double value) {
+void log_benchmark_data(char* benchmark_name, char* unit, double value) {
     printf("/*BENCHMARK OUTPUT*/\nname: %s\nunit: %s\nvalue: %f\nplot_group: \n/*BENCHMARK OUTPUT END*/\n", benchmark_name, unit, value);
 }
 
@@ -15,7 +15,7 @@ void log_benchmark_data(const char* benchmark_name, const char* unit, double val
 /// * `unit` - The unit of the benchmark value.
 /// * `value` - The benchmark value.
 /// * `plot_group` - The plot group in which the benchmark will be plotted.
-void log_benchmark_data_with_group(const char* benchmark_name, const char* unit, double value, const char* plot_group) {
+void log_benchmark_data_with_group(char* benchmark_name, char* unit, double value, char* plot_group) {
     printf("/*BENCHMARK OUTPUT*/\nname: %s\nunit: %s\nvalue: %f\nplot_group: %s\n/*BENCHMARK OUTPUT END*/\n", benchmark_name, unit, value, plot_group);
 }
 
--- usr/benchmarks/bench_output.h
+++ usr/benchmarks/bench_output.h
@@ -5,7 +5,7 @@
 /// * `benchmark_name` - The name under which the benchmark will be stored.
 /// * `unit` - The unit of the benchmark value.
 /// * `value` - The benchmark value.
-void log_benchmark_data(const char* benchmark_name, const char* unit, double value);
+void log_benchmark_data(char* benchmark_name, char* unit, double value);
 
 /// Log benchmark data to stdout, so it can be captured by the benchmarking framework hermit-bench.
 ///
@@ -13,5 +13,5 @@ void log_benchmark_data(const char* benchmark_name, const char* unit, double val
 /// * `unit` - The unit of the benchmark value.
 /// * `value` - The benchmark value.
 /// * `plot_group` - The plot group in which the benchmark will be plotted.
-void log_benchmark_data_with_group(const char* benchmark_name, const char* unit, double value, const char* plot_group);
+void log_benchmark_data_with_group(char* benchmark_name, char* unit, double value, char* plot_group);
 
