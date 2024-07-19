3642490b76c0f41c27f644cd7daeb97fa37c96e0
CarlWachter <carlwachter@web.de>
2024-07-19 13:37:46 +0200

    bench: added mutex bench

---
+++ usr/benchmarks/mutex/main.c
@@ -0,0 +1,116 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdatomic.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include "../bench_output.h"
+
+#define NUMBER_OF_ITERATIONS 10000000
+#define NUM_THREADS 2
+
+typedef struct {
+    atomic_size_t num_threads;
+} SpinBarrier;
+
+typedef struct {
+    SpinBarrier *barrier;
+    pthread_mutex_t *mutex;
+    size_t *counter;
+    uint64_t duration;
+} ThreadArgs;
+
+void spin_barrier_init(SpinBarrier *barrier, size_t n) {
+    atomic_init(&barrier->num_threads, n);
+}
+
+void spin_barrier_wait(SpinBarrier *barrier) {
+    atomic_fetch_sub_explicit(&barrier->num_threads, 1, memory_order_relaxed);
+    while (atomic_load_explicit(&barrier->num_threads, memory_order_relaxed) != 0) {
+        __builtin_ia32_pause();
+    }
+}
+
+uint64_t get_time_ns() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (uint64_t)tv.tv_sec * 1000000000 + (uint64_t)tv.tv_usec * 1000;
+}
+
+void *mutex_stress_test_thread(void *arg) {
+    ThreadArgs *args = (ThreadArgs *)arg;
+    uint64_t start, end;
+
+    // Warmup
+    start = get_time_ns();
+    for (size_t i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+        pthread_mutex_lock(args->mutex);
+        (*args->counter)++;
+        pthread_mutex_unlock(args->mutex);
+    }
+    end = get_time_ns();
+    args->duration = end - start;
+    spin_barrier_wait(args->barrier);
+
+    start = get_time_ns();
+    for (size_t i = 0; i < NUMBER_OF_ITERATIONS; i++) {
+        pthread_mutex_lock(args->mutex);
+        (*args->counter)++;
+        pthread_mutex_unlock(args->mutex);
+    }
+    end = get_time_ns();
+    args->duration = end - start;
+
+    return NULL;
+}
+
+void mutex_stress_test(size_t no_threads) {
+    size_t counter = 0;
+    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+    SpinBarrier barrier;
+    spin_barrier_init(&barrier, no_threads);
+
+    pthread_t *threads = malloc(no_threads * sizeof(pthread_t));
+    ThreadArgs *args = malloc(no_threads * sizeof(ThreadArgs));
+
+    for (size_t i = 0; i < no_threads; i++) {
+        args[i].barrier = &barrier;
+        args[i].mutex = &mutex;
+        args[i].counter = &counter;
+        pthread_create(&threads[i], NULL, mutex_stress_test_thread, &args[i]);
+    }
+
+    uint64_t total_duration = 0;
+    for (size_t i = 0; i < no_threads; i++) {
+        pthread_join(threads[i], NULL);
+        total_duration += args[i].duration;
+    }
+
+    free(threads);
+    free(args);
+
+    if (counter != 2 * NUMBER_OF_ITERATIONS * no_threads) {
+        fprintf(stderr, "Counter value is incorrect: %zu\n", counter);
+        exit(EXIT_FAILURE);
+    }
+    
+    char bench_name[50];    
+    sprintf(bench_name, "%d Threads", no_threads);
+
+    double average = (double)total_duration / (no_threads * NUMBER_OF_ITERATIONS);
+    log_benchmark_data_with_group(
+        bench_name,
+        "ns",
+        average,
+        (char *)"Mutex Stress Test Average Time per Iteration"
+    );
+}
+
+int main() {
+    for(int i = 1; i <= NUM_THREADS; i*=2) {
+        mutex_stress_test(i);
+    }
+
+    return 0;
+}
